965011b4ee1295055f760aaecbb43d589ee731a6
LorenzoAita <76094757+LorenzoAita@users.noreply.github.com>
2022-09-06 15:42:04 +0200

    Update Watchdog.py

--- Watchdog.py
+++ Watchdog.py
@@ -4,7 +4,7 @@ import os
 def main():
     path_watchdog = r'S:\@Solar\Reliability Laboratory\0_Stazioni di Test\10_AITA\0_misc/watchdog.txt'
     file_object = open(path_watchdog, "r")
-    while file_object.readline() in ['0', '4']:
+    while file_object.readline() in ['0', '4', 'test']:
         time.sleep(5)
         file_object.close()
         file_object = open(path_watchdog, "r")
@@ -24,7 +24,7 @@ def main():
         os.system('wmic process where "commandline like \'%%TestSeq.py%%\'" delete')
     if file_object.readline() == '3':
         print('l\'errore è nella visualizzazione dei dati!')
-        #provo a farli ripartire
+        # provo a farli ripartire
         os.system('python Watchdog.py VisulaDati.py')
 
 main()
