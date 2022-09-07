ab0827a4e9c4a5694366fbd5274fc2ed8d6a710b
LorenzoAita <76094757+LorenzoAita@users.noreply.github.com>
2022-09-07 17:12:55 +0200

    aggiornamento watchdog

--- LogAITA.py
+++ LogAITA.py
@@ -236,7 +236,7 @@ def main_log():
 
         print('>>> End')
     except:
-        path_watchdog = r'S:\@Solar\Reliability Laboratory\0_Stazioni di Test\10_AITA\0_misc/watchdog.txt'
+        path_watchdog = './misc/watchdog.txt'
         file_object = open(path_watchdog, "w")
         file_object.write('1')
         file_object.close()
--- TestSeq.py
+++ TestSeq.py
@@ -335,7 +335,7 @@ def main_test():
             else:
                 a = False
     except:
-        path_watchdog = r'S:\@Solar\Reliability Laboratory\0_Stazioni di Test\10_AITA\0_misc/watchdog.txt'
+        path_watchdog = './misc/watchdog.txt'
         file_object = open(path_watchdog, "w")
         file_object.write('2')
         file_object.close()
